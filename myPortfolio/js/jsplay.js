660c43536e238781113da2ad80ec2561f3498ae2
lbpicon <lrn.picon@gmail.com>
2019-04-10 18:00:43 -0500

    committed

---
+++ myPortfolio/js/jsplay.js
@@ -0,0 +1,11 @@
+var myName = prompt("Enter Your Name", "");
+
+//if the name is Lauren Picon
+if (myName == "Lauren Picon")
+    {
+        alert("Hello Me, Forget about dat Loser")
+    }
+else {
+    alert("Hello whoever u are")
+    
+}
